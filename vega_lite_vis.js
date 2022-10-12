5f962f39becfb35b535aeefe1d2c868fff07053f
Ethan0017 <62064001+Ethan0017@users.noreply.github.com>
2022-10-13 04:59:37 +1100

    uploading draft files

---
+++ vega_lite_vis.js
@@ -0,0 +1,25 @@
+var vg_1 = "cheapest_bar.vg.json";
+
+vegaEmbed("#bar_chart", vg_1).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+
+
+var vg_2 = "lineChart.json";
+vegaEmbed("#line_chart", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
+
+
+
+var vg_3 = "pieChart.json";
+vegaEmbed("#pie_chart", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
+
+
+var vg_4 = "scatter.json";
+vegaEmbed("#scatter_chart", vg_4).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
